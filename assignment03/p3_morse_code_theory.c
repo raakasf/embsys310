974d7f79892f3c6674b4eb632edc3ca2f8a3105b
R S <4614260+raakasf@users.noreply.github.com>
2021-11-01 04:57:50 +0000

    Morse code definition

---
+++ assignment03/p3_morse_code_theory.c
@@ -0,0 +1,12 @@
+# About Morse Code 
+
+## a. What is Morse Code?
+
+In telecommunications, more code is a method to encode text as a two signals of 
+different durations called dots and dashes.
+
+## b. Who developed Morse Code?
+
+Morse code evolved over a couple of decades but it was names after Samuel Morse (1837) and the 
+final revision that is known today as as internation standard was derived by 
+Friedrich Gerke (1848)
